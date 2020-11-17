83a710afdc08701f4a64b2728e3d4bfbb8c96e86
Aiden Dai  <daixb@amazon.com>
2020-11-17 20:22:16 +0800

    Add api of listing ssm parameters

--- source/constructs/lambda/api/api-ssm-param.ts
+++ source/constructs/lambda/api/api-ssm-param.ts
@@ -2,13 +2,20 @@ import * as AWS from "aws-sdk";
 import { Context } from "aws-lambda";
 import { pprint } from '../common';
 
-const handler = function (event: any, context: Context, callback: any) {
+
+type Parameter = {
+    name?: string,
+    type?: string,
+    description?: string
+}
+
+const handler = async function (event: any, context?: Context) {
 
     pprint('EVENT', event)
     pprint('CONTEXT', context)
 
     const ssm = new AWS.SSM({ apiVersion: '2014-11-06' });
-    let values: Array<any> = [];
+    let result: Array<Parameter> = [];
 
     const params = {
         Filters: [
@@ -19,20 +26,23 @@ const handler = function (event: any, context: Context, callback: any) {
         ]
 
     };
-    ssm.describeParameters(params, function (err, data) {
+    await ssm.describeParameters(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else {
             console.log(data);
             if (data.Parameters) {
                 for (const d of data.Parameters) {
-                    values.push({ "name": d.Name, "type": d.Type, "description": d.Description });
+                    result.push({
+                        name: d.Name,
+                        type: d.Type,
+                        description: d.Description
+                    });
                 }
-                callback(null, values)
             }
         }
-    });
+    }).promise();
 
-
+    return result
 }
 
 export { handler }
