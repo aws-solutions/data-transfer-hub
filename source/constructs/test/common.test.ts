ba7f0de84537ba46fa6b860fc2a1b78c578e9d36
Kervin Hu <huyikai@amazon.com>
2021-12-26 20:59:50 +0800

    docs: init the github pages

--- source/constructs/test/common.test.ts
+++
@@ -1,6 +0,0 @@
-import { makeid } from "../lambda/common";
-
-test('makeid function', () => {
-  const id = makeid(10)
-  expect(id.search('([A-Z]|[a-z]){10}')).toEqual(0)
-})
--- source/portal/src/common/context/ClientContext.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-const ClientContext = React.createContext("");
-export default ClientContext;
